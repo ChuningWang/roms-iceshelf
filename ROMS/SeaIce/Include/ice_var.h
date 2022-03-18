3dc8c10d6974fd27b750c7f93d58796364856f0e
Chuning Wang <chuning@marine.rutgers.edu>
2022-03-18 13:54:22 +0800

    Adjust ice model I/O scheme. Begin to work on a sedment/biology style I/O.

---
+++ ROMS/SeaIce/Include/ice_var.h
@@ -0,0 +1,238 @@
+#ifdef ICE_MODEL
+            CASE ('idsnow')
+              idsnow=varid
+            CASE ('idIuice')
+              idIuice=varid
+            CASE ('idIvice')
+              idIvice=varid
+            CASE ('idIuiceE')
+              idIuiceE=varid
+            CASE ('idIviceN')
+              idIviceN=varid
+            CASE ('idIaice')
+              idIaice=varid
+            CASE ('idIhice')
+              idIhice=varid
+            CASE ('idIhsno')
+              idIhsno=varid
+            CASE ('idItice')
+              idItice=varid
+            CASE ('idItmid')
+              idItmid=varid
+            CASE ('idIt2ic')
+              idIt2ic=varid
+            CASE ('idIutau')
+              idIutau=varid
+            CASE ('idImchu')
+              idImchu=varid
+            CASE ('idIs0mk')
+              idIs0mk=varid
+            CASE ('idIt0mk')
+              idIt0mk=varid
+            CASE ('idIwfr')
+              idIwfr=varid
+            CASE ('idIwai')
+              idIwai=varid
+            CASE ('idIwao')
+              idIwao=varid
+            CASE ('idIwio')
+              idIwio=varid
+            CASE ('idIwro')
+              idIwro=varid
+            CASE ('idIwdiv')
+              idIwdiv=varid
+            CASE ('idIagei')
+              idIagei=varid
+            CASE ('idImflx')
+              idImflx=varid
+            CASE ('idIsig')
+              idIsig=varid
+            CASE ('idIsg11')
+              idIsg11=varid
+            CASE ('idIsg22')
+              idIsg22=varid
+            CASE ('idIsg12')
+              idIsg12=varid
+            CASE ('idAIclm')
+              idAIclm=varid
+            CASE ('idHIclm')
+              idHIclm=varid
+            CASE ('idHSNclm')
+              idHSNclm=varid
+            CASE ('idS11clm')
+              idS11clm=varid
+            CASE ('idS12clm')
+              idS12clm=varid
+            CASE ('idS22clm')
+              idS22clm=varid
+            CASE ('idUIclm')
+              idUIclm=varid
+            CASE ('idVIclm')
+              idVIclm=varid
+            CASE ('idFastIce')
+              idFastIce=varid
+            CASE ('idIalbe')
+              idIalbe=varid
+            CASE ('idIalbI')
+              idIalbI=varid
+            CASE ('idIlrdn')
+              idIlrdn=varid
+            CASE ('idIsrdn')
+              idIsrdn=varid
+            CASE ('idIlatI')
+              idIlatI=varid
+            CASE ('idIsenI')
+              idIsenI=varid
+            CASE ('idIlrup')
+              idIlrup=varid
+            CASE ('idIsrup')
+              idIsrup=varid
+            CASE ('idIsrin')
+              idIsrin=varid
+            CASE ('idIsrth')
+              idIsrth=varid
+            CASE ('idIsfI')
+              idIsfI=varid
+            CASE ('idIsfL')
+              idIsfL=varid
+            CASE ('idIqao')
+              idIqao=varid
+            CASE ('idIqai')
+              idIqai=varid
+            CASE ('idIqio')
+              idIqio=varid
+            CASE ('idIqi2')
+              idIqi2=varid
+            CASE ('idIsnoi')
+              idIsnoi=varid
+            CASE ('idIfrsw')
+              idIfrsw=varid
+            CASE ('idIrfac')
+              idIrfac=varid
+            CASE ('idAibc(iwest)')
+              idAibc(iwest)=varid
+            CASE ('idAibc(ieast)')
+              idAibc(ieast)=varid
+            CASE ('idAibc(isouth)')
+              idAibc(isouth)=varid
+            CASE ('idAibc(inorth)')
+              idAibc(inorth)=varid
+            CASE ('idHibc(iwest)')
+              idHibc(iwest)=varid
+            CASE ('idHibc(ieast)')
+              idHibc(ieast)=varid
+            CASE ('idHibc(isouth)')
+              idHibc(isouth)=varid
+            CASE ('idHibc(inorth)')
+              idHibc(inorth)=varid
+            CASE ('idHsnbc(iwest)')
+              idHsnbc(iwest)=varid
+            CASE ('idHsnbc(ieast)')
+              idHsnbc(ieast)=varid
+            CASE ('idHsnbc(isouth)')
+              idHsnbc(isouth)=varid
+            CASE ('idHsnbc(inorth)')
+              idHsnbc(inorth)=varid
+            CASE ('idTibc(iwest)')
+              idTibc(iwest)=varid
+            CASE ('idTibc(ieast)')
+              idTibc(ieast)=varid
+            CASE ('idTibc(isouth)')
+              idTibc(isouth)=varid
+            CASE ('idTibc(inorth)')
+              idTibc(inorth)=varid
+            CASE ('idApdbc(iwest)')
+              idApdbc(iwest)=varid
+            CASE ('idApdbc(ieast)')
+              idApdbc(ieast)=varid
+            CASE ('idApdbc(isouth)')
+              idApdbc(isouth)=varid
+            CASE ('idApdbc(inorth)')
+              idApdbc(inorth)=varid
+            CASE ('idHpdbc(iwest)')
+              idHpdbc(iwest)=varid
+            CASE ('idHpdbc(ieast)')
+              idHpdbc(ieast)=varid
+            CASE ('idHpdbc(isouth)')
+              idHpdbc(isouth)=varid
+            CASE ('idHpdbc(inorth)')
+              idHpdbc(inorth)=varid
+            CASE ('idAgeicebc(iwest)')
+              idAgeicebc(iwest)=varid
+            CASE ('idAgeicebc(ieast)')
+              idAgeicebc(ieast)=varid
+            CASE ('idAgeicebc(isouth)')
+              idAgeicebc(isouth)=varid
+            CASE ('idAgeicebc(inorth)')
+              idAgeicebc(inorth)=varid
+            CASE ('idUibc(iwest)')
+              idUibc(iwest)=varid
+            CASE ('idUibc(ieast)')
+              idUibc(ieast)=varid
+            CASE ('idUibc(isouth)')
+              idUibc(isouth)=varid
+            CASE ('idUibc(inorth)')
+              idUibc(inorth)=varid
+            CASE ('idVibc(iwest)')
+              idVibc(iwest)=varid
+            CASE ('idVibc(ieast)')
+              idVibc(ieast)=varid
+            CASE ('idVibc(isouth)')
+              idVibc(isouth)=varid
+            CASE ('idVibc(inorth)')
+              idVibc(inorth)=varid
+            CASE ('idS11bc(iwest)')
+              idS11bc(iwest)=varid
+            CASE ('idS11bc(ieast)')
+              idS11bc(ieast)=varid
+            CASE ('idS11bc(isouth)')
+              idS11bc(isouth)=varid
+            CASE ('idS11bc(inorth)')
+              idS11bc(inorth)=varid
+            CASE ('idS22bc(iwest)')
+              idS22bc(iwest)=varid
+            CASE ('idS22bc(ieast)')
+              idS22bc(ieast)=varid
+            CASE ('idS22bc(isouth)')
+              idS22bc(isouth)=varid
+            CASE ('idS22bc(inorth)')
+              idS22bc(inorth)=varid
+            CASE ('idS12bc(iwest)')
+              idS12bc(iwest)=varid
+            CASE ('idS12bc(ieast)')
+              idS12bc(ieast)=varid
+            CASE ('idS12bc(isouth)')
+              idS12bc(isouth)=varid
+            CASE ('idS12bc(inorth)')
+              idS12bc(inorth)=varid
+#endif
+#ifdef ICESHELF
+            CASE ('idIsMr')
+              idIsMr=varid
+            CASE ('idIsTs')
+              idIsTs=varid
+            CASE ('idIsUs')
+              idIsUs=varid
+            CASE ('idIsTb')
+              idIsTb=varid
+            CASE ('idIsSb')
+              idIsSb=varid
+            CASE ('idIsDrft')
+              idIsDrft=varid
+            CASE ('idIsVol')
+              idIsVol=varid
+            CASE ('idIsSOFs')
+              idIsSOFs=varid
+            CASE ('idIsSOFh')
+              idIsSOFh=varid
+            CASE ('idIsTrc(itemp)')
+              idIsTrc(itemp)=varid
+# ifdef SALINITY
+            CASE ('idIsTrc(isalt)')
+              idIsTrc(isalt)=varid
+# endif
+# ifdef T_PASSIVE
+            CASE ('idIsTrc(inert(i))')
+              load=.TRUE.
+# endif
+#endif
