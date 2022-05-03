64ed47505cf66d33e410d76e9f8db4a8b806bb14
Chuning Wang <wangchuning@sjtu.edu.cn>
2022-05-03 23:06:59 +0800

    Some modification on processing tracers for iceshelf melt water.

--- ROMS/IceShelf/Module.mk
+++ ROMS/IceShelf/Module.mk
@@ -1,15 +1,13 @@
-# svn $Id$
 #::::::::::::::::::::::::::::::::::::::::::::::::::::: Hernan G. Arango :::
-#   Copyright (c) 2002-2013 The ROMS/TOMS Group                         :::
+#   Copyright (c) 2002-2022 The ROMS/TOMS Group            Chuning Wang :::
 #   Licensed under a MIT/X style license                                :::
 #   See License_ROMS.txt                                                :::
 #::::::::::::::::::::::::::::::::::::::::::::: Benjamin K. Galton-Fenzi :::
 
 local_sub  := ROMS/IceShelf
 
-local_lib  := libICESHELF.a
 local_src  := $(wildcard $(local_sub)/*.F)
 
-$(eval $(call make-library,$(local_lib),$(local_src)))
+sources    += $(local_src)
 
 $(eval $(compile-rules))
